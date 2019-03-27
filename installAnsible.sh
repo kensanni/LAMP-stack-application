a0fc3e2b3b86837fc6e7c4dc2ab6d100ab3d90e1
kensanni <kensanni@gmail.com>
2019-03-27 19:36:45 +0100

    Feat(Ansible): Automate the deployment process using ansible
    Setup ansible
    Create ansible playbook
    Create inventory file
    create wordpress yml file
    
    [Finishes #164938516]

---
+++ installAnsible.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+installAnsible () {
+  sudo apt-get update
+  sudo apt-get install software-properties-common -y
+  sudo apt-add-repository ppa:ansible/ansible
+  sudo apt-get update
+  sudo apt-get install ansible -y
+}
+
+main () {
+  installAnsible
+}
+
+main
